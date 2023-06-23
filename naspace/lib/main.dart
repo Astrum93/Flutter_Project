50421886292baabb8464df050f303a22cde84329
Astrum <jungmin2670@gmail.com>
2023-06-24 00:03:42 +0900

    230623

--- naspace/lib/main.dart
+++ naspace/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:naspace/Screen/WelcomScreen.dart';
+import 'package:naspace/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const NAspace());
 }
 
