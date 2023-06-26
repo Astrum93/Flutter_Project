d430ddf08da8b93a5427a391903f0f9a2eb858fd
Astrum <jungmin2670@gmail.com>
2023-06-27 01:18:34 +0900

    230627

--- naspace/lib/Screen/JoinScreen.dart
+++ naspace/lib/Screen/JoinScreen.dart
@@ -1,30 +1,222 @@
 import 'package:flutter/material.dart';
 
-class JoinScreen extends StatelessWidget {
+class JoinScreen extends StatefulWidget {
   const JoinScreen({super.key});
+
+  @override
+  State<JoinScreen> createState() => _JoinScreenState();
+}
+
+class _JoinScreenState extends State<JoinScreen> {
+  // Form Key
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.transparent,
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 15, 15, 15).withOpacity(0.3),
-        foregroundColor: Colors.white,
-        centerTitle: true,
-        title: const Text('Welcome Music is Life'),
+        backgroundColor: Colors.transparent,
       ),
-      body: const Center(
-        child: Column(
-          children: [
-            SizedBox(height: 30),
-            Text(
-              '회원가입',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Image(
+                  image: const AssetImage('lib/Image/Icon/vr-headset.png'),
+                  // 화면 크기의 -10만큼의 가로 길이
+                  width: MediaQuery.of(context).size.width - 10,
+                ),
               ),
-            ),
-          ],
+              Container(
+                decoration: const BoxDecoration(color: Colors.black),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+
+                    // Form
+                    child: Form(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          const Center(
+                            child: Text(
+                              '회원가입',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+
+                          // 이름
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.account_circle,
+                                color: Colors.grey,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(35),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(35),
+                                ),
+                              ),
+                              hintText: "이름",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+
+                          // 이메일
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.mail,
+                                color: Colors.grey,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(35),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(35),
+                                ),
+                              ),
+                              hintText: "메일",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+
+                          // 비밀번호
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.password_rounded,
+                                color: Colors.grey,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(35),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(35),
+                                ),
+                              ),
+                              hintText: "비밀번호",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+
+                          // 전화 번호
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.phone_android_rounded,
+                                color: Colors.grey,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(35),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(35),
+                                ),
+                              ),
+                              hintText: "전화번호",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          InkWell(
+                            onTap: () {
+                              //
+                            },
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              ),
+                              child: const Center(
+                                child: Icon(
+                                  Icons.check,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '또는 소셜 로그인',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 10,
+                                ),
+                              ),
+                              SizedBox(height: 30),
+                            ],
+                          ),
+                          TextButton.icon(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                minimumSize: const Size(120, 40),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                backgroundColor: Colors.orange.shade900),
+                            icon: const Icon(Icons.add),
+                            label: const Text('Google'),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- naspace/lib/Screen/LogInScreen.dart
+++ naspace/lib/Screen/LogInScreen.dart
@@ -8,6 +8,8 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
+  // Form Key
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,29 +17,109 @@ class _LogInScreenState extends State<LogInScreen> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
       ),
-      body: Column(
-        children: [
-          const Center(
-            child: Image(
-              image: AssetImage('lib/Image/Icon/Login.png'),
-            ),
-          ),
-          const SizedBox(height: 50),
-          Container(
-            decoration: const BoxDecoration(color: Colors.white),
-            child: const Row(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: 'e-mail',
-                      border: OutlineInputBorder(),
-                      fillColor: Colors.white),
-                  keyboardType: TextInputType.emailAddress,
+
+      // 입력 제외 부분 Tap하면 키보드 사라지게
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // 로그인 메인 이미지
+              const Center(
+                child: Image(
+                  image: AssetImage('lib/Image/Icon/Login.png'),
                 ),
-              ],
-            ),
-          )
-        ],
+              ),
+              const SizedBox(height: 50),
+
+              //
+              Container(
+                decoration: const BoxDecoration(color: Colors.black),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Form(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          const Row(
+                            children: [
+                              Text(
+                                '이메일',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 15),
+
+                          // 아이디 입력
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'e-mail',
+                                border: OutlineInputBorder(),
+                                filled: true,
+                                fillColor: Colors.white),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          const SizedBox(height: 15),
+
+                          const Row(
+                            children: [
+                              Text(
+                                '비밀번호',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 15),
+
+                          // 비밀번호 입력
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'password',
+                                border: OutlineInputBorder(),
+                                filled: true,
+                                fillColor: Colors.white),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          const SizedBox(height: 35),
+
+                          // 체크 버튼
+                          InkWell(
+                            onTap: () {
+                              //
+                            },
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              ),
+                              child: const Center(
+                                child: Icon(
+                                  Icons.check,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
