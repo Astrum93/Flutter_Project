45975325259b4d88e2506b93cfb7f2709ce47bfc
Astrum93 <jungmin2670@gmail.com>
2023-08-09 23:42:01 +0900

    230809

--- naspace/lib/Screen/MyScreen.dart
+++ naspace/lib/Screen/MyScreen.dart
@@ -448,7 +448,7 @@ class _MyScreenState extends State<MyScreen> {
 
                             // 하위 컬렉션 문서들을 리스트로 표시
                             var subcollectionDocs = snapshot.data!.docs;
-
+                            // GridView.builder
                             return SizedBox(
                               width: MediaQuery.of(context).size.width,
                               height: 220,
