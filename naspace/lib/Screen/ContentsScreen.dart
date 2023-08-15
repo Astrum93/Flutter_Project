442ee06a2b47133403baa3a58bfe588fe675f9b9
Astrum93 <jungmin2670@gmail.com>
2023-08-15 23:23:08 +0900

    230815

--- naspace/lib/Screen/ContentsScreen.dart
+++ naspace/lib/Screen/ContentsScreen.dart
@@ -17,70 +17,75 @@ class _contentsScreenState extends State<contentsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            // User 게시물
-            StreamBuilder(
-              stream: FirebaseFirestore.instance
-                  .collection('UserContents')
-                  .doc(_uid)
-                  .collection('Contents')
-                  .snapshots(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                }
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              // User 게시물
+              StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('UserContents')
+                    .doc(_uid)
+                    .collection('Contents')
+                    .snapshots(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  }
 
-                // 하위 컬렉션 문서들을 리스트로 표시
-                var subcollectionDocs = snapshot.data!.docs;
+                  // 하위 컬렉션 문서들을 리스트로 표시
+                  var subcollectionDocs = snapshot.data!.docs;
 
-                // ListView.builder 담을 SizedBox
-                return SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    itemBuilder: (Context, index) {
-                      var doc = subcollectionDocs[index];
-                      var fieldValue = doc.get('ContentsImage');
+                  // ListView.builder 담을 SizedBox
+                  return SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (Context, index) {
+                        var doc = subcollectionDocs[index];
+                        var fieldValue = doc.get('ContentsImage');
 
-                      // User 게시물들 일렬로 나열
-                      return Column(
-                        children: [
-                          const SizedBox(height: 20),
-                          // 유저 프로필 사진과 유저 아이디
-                          const Padding(
-                            padding: EdgeInsets.only(left: 4),
-                            child: Row(
-                              children: [
-                                Text(
-                                  'UserName',
-                                  style: TextStyle(color: Colors.grey),
-                                )
-                              ],
-                            ),
-                          ),
-                          // 컨텐츠 이미지
-                          Container(
-                            width: MediaQuery.of(context).size.width - 8,
-                            height: 250,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(fieldValue),
+                        // User 게시물들 일렬로 나열
+                        return Column(
+                          children: [
+                            const SizedBox(height: 20),
+                            // 유저 프로필 사진과 유저 아이디
+                            const Padding(
+                              padding: EdgeInsets.only(left: 4),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    'UserName',
+                                    style: TextStyle(color: Colors.grey),
+                                  )
+                                ],
                               ),
                             ),
-                            child: const Text(''),
-                          ),
-                          const SizedBox(height: 80)
-                        ],
-                      );
-                    },
-                  ),
-                );
-              },
-            )
-          ],
+                            // 컨텐츠 이미지
+                            Container(
+                              width: MediaQuery.of(context).size.width - 8,
+                              height: 250,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(fieldValue),
+                                ),
+                              ),
+                              child: const Text(''),
+                            ),
+                            const SizedBox(height: 80)
+                          ],
+                        );
+                      },
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
         ));
   }
 }
