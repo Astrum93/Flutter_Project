9d3762493fb00ea3cf13d96e33fcca98f9af9f45
Astrum93 <jungmin2670@gmail.com>
2023-09-06 23:00:32 +0900

    230906

--- naspace/lib/Screen/ContentsScreen.dart
+++ naspace/lib/Screen/ContentsScreen.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:naspace/Screen/ContentsScreen1.dart';
+import 'package:naspace/Widget/MusicPlayer.dart';
 
 import '../Widget/ShortContainerLine.dart';
 
